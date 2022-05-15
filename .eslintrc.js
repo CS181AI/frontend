556b908cbde7069b868a05ca24ec43afd864ee9a
Fly <1756339062@qq.com>
2022-05-15 12:58:48 +0800

    更改了一些eslint配置 no-restricted-syntax no-constant-condition no-param-reassign

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,6 +36,9 @@ module.exports = {
     'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
     // 关闭class中函数this检查
     'class-methods-use-this': 'off',
+    'no-restricted-syntax': 'off',
+    'no-constant-condition': 'off',
+    'no-param-reassign': 'off',
   },
   settings: {
     // 安装eslint-import-resolver-typescript后在此配置import/resolver，即可读取项目根目录下tsconfig.json中的路径别名设置
